49d05c40913aad8d515ecf20600a9f69d68c1c3f
Jhonatan Jair Garcia Alfaro <40862403+joga47uk@users.noreply.github.com>
2023-03-20 12:39:38 -0500

    initial commit

--- products.js
+++ products.js
@@ -3,7 +3,9 @@
 const contentProd = document.querySelector(".contentProd")
 contentProd.innerHTML=``
 
+//array productos
 
+var productos = []
 
 //Mostrar todos los productos
 
@@ -21,11 +23,41 @@ Promise.resolve(ProducAjax()).then(item=>{
             <div class="card-body">
                 <h5 class="card-title">${res.name}</h5>
                 <h6 class="card-subtitle mb-2 text-muted ">$${res.prece}</h6>
-                <button class="btn btn-primary agregarProducto" >Agregar</button>
+                <button class="btn btn-primary agregarProducto" id_producto="${res.id}" img="${res.img}" name="${res.name}" precio="${res.prece}">Agregar</button>
             </div>
         </div>
     </div>`
     })
+    agregarProductoC()
 
     
 })
+
+//capturar valores de los productos 
+function agregarProductoC(){
+    const agregarProducto = document.querySelectorAll(".agregarProducto")
+const mapbuttom = [...agregarProducto]
+mapbuttom.map(items=>{
+    items.addEventListener("click", btnAgregarCarrito)
+})
+}
+
+
+//funcion agregar al carrito localStorage
+function btnAgregarCarrito(e){
+    const producto = {
+        "id": parseInt(e.target.getAttribute("id_producto")),
+        "name": e.target.getAttribute("name"),
+        "img": e.target.getAttribute("img"),
+        "prece": parseInt(e.target.getAttribute("precio"))
+    }
+
+    if(productos.length == 0){
+        productos.push(producto)
+        console.log(productos)
+    }else{
+       productos.filter(item=>parseInt(item.id) == parseInt(producto.id) ? false : productos.push(producto))
+       console.log(productos)
+        
+    }
+}
